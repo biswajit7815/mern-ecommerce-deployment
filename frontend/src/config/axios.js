33fdb6f9cfb0644ad846a7a92c5422e3760d99fc
root <root@ip-172-31-40-13.ap-south-1.compute.internal>
2026-05-13 21:00:12 +0000

    commited all correct code and push in github

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ? ''
-  : 'http://localhost:4000';
+  ? '/api'
+  : 'http://localhost:8000';
 
 export const axiosi = axios.create({
   withCredentials: true,
